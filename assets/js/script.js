1df789917877aeef61f1f9fd2b9f721a834a3999
ViictorWebTech <vwebtech777@gmail.com>
2023-07-15 14:03:47 -0300

    :ok_hand: style: Código formatado e espaçado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,42 @@
 const columns = document.querySelectorAll(".column");
 
 document.addEventListener("dragstart", (e) => {
-  e.target.classList.add("dragging");
+    e.target.classList.add("dragging");
 });
 
 document.addEventListener("dragend", (e) => {
-  e.target.classList.remove("dragging");
+    e.target.classList.remove("dragging");
 });
 
 columns.forEach((item) => {
-  item.addEventListener("dragover", (e) => {
-    const dragging = document.querySelector(".dragging");
-    const applyAfter = getNewPosition(item, e.clientY);
+    item.addEventListener("dragover", (e) => {
+        const dragging = document.querySelector(".dragging");
+        const applyAfter = getNewPosition(item, e.clientY);
 
-    if (applyAfter) {
-      applyAfter.insertAdjacentElement("afterend", dragging);
-    } else {
-      item.prepend(dragging);
-    }
-  });
+        if (applyAfter) {
+            applyAfter.insertAdjacentElement("afterend", dragging);
+        } else {
+            item.prepend(dragging);
+        }
+    });
 });
 
+/**
+ * Função para pegar o elemento selecionado e mover para outra coluna
+ * @param column marca a coluna que está passando
+ * @param posY marca a posição que vai assumir no eixo Y
+ * @returns O resultado é o elemento que está abaixo do mouse
+ */
 function getNewPosition(column, posY) {
-  const cards = column.querySelectorAll(".item:not(.dragging)");
-  let result;
+    const cards = column.querySelectorAll(".item:not(.dragging)");
+    let result;
 
-  for (let refer_card of cards) {
-    const box = refer_card.getBoundingClientRect();
-    const boxCenterY = box.y + box.height / 2;
+    for (let refer_card of cards) {
+        const box = refer_card.getBoundingClientRect();
+        const boxCenterY = box.y + box.height / 2;
 
-    if (posY >= boxCenterY) result = refer_card;
-  }
+        if (posY >= boxCenterY) result = refer_card;
+    }
 
-  return result;
+    return result;
 }
